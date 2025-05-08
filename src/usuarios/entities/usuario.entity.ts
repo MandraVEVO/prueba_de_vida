fe34591b76e81d192bef632673418835e8ee98f2
NeriG03 <sosavillanuevan@gmail.com>
2025-05-08 15:17:01 -0600

    Implementacion pruebas
    Se realizo la entidad y la relacion con usuarios

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,21 +1,24 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Prueba } from 'src/prueba/entities/prueba.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Usuario {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    nombre: string;
-    @Column()
-    apellidopa: string;
-    @Column()
-    apellidoma: string;
-    @Column({
-        unique: true,
-    })
-    email: string;
-    @Column()
-    password: string;
-    
+  @Column()
+  nombre: string;
+  @Column()
+  apellidopa: string;
+  @Column()
+  apellidoma: string;
+  @Column({
+    unique: true,
+  })
+  email: string;
+  @Column()
+  password: string;
+
+  @OneToMany(() => Prueba, (prueba) => prueba.usuario)
+  pruebas: Prueba[];
 }
